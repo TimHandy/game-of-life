afec28bf6a6ce153580852142e5988b33316ed68
Parminder Sanghera <gambit@Bilgas-MacBook-Pro.local>
2017-01-26 10:04:53 +0000

    additional 6 successful tests have been added. These include that a cell can be selected by default; a cell knows it adjacent cells; it can count how many adjacent cells are selected

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,19 +1,70 @@
 describe('game', function() {
 
   var game;
-
+  var cell;
+  var board;
+  var rows = 100;
+  var cols = 100;
 
   describe('board', function() {
 
     beforeEach(function() {
-      game = new Game();
+      game = new Game(rows, cols);
+      board = game.board
+      game.start()
     });
 
     it('should be an array', function() {
-      expect ( Array.isArray(game.board) ).toBe(true)
+      expect ( Array.isArray(board) ).toBe(true)
     })
 
+    it('should be a 100 x 100 array', function() {
+      game = new Game(100, 100);
+      expect ( game.board.length ).toBe(100)
+      expect ( game.board[0].length ).toBe(100)
+    })
+
+    it('should contain a cell object', function() {
+      expect ( typeof board[1][1]).toBe('object')
+    })
+
+    it('cell [1][5] is selected on by default', function() {
+      expect (board[1][5].selected).toBe(true)
+    })
+
+    it('cell [1,4] default selected adjacent count is 1', function() {
+      cell = game.board[1][4];
+      expect (game.countSelectedAdjacents(cell)).toBe(1);
+    })
+
+    it('cell [0,0] default selected adjacent count is 0', function() {
+      cell = game.board[0][0];
+      expect (game.countSelectedAdjacents(cell)).toBe(0);
+    })
 
   });
+
+    describe('cell', function() {
+
+      beforeEach(function() {
+        game = new Game(rows, cols);
+        game.start();
+        cell = game.board[0][0];
+      });
+
+      it('can be selected on (true) or off (false)', function() {
+        expect ( typeof cell.selected).toBe('boolean')
+      })     
+
+      it('[2,2] adjacent cells are [1,2],[3,2],[2,3],[2,1],[1,1],[3,3],[1,3],[3,1]', function() {
+        cell = game.board[2][2];
+        expect (JSON.stringify(cell.getAdjacents([2,2], cell.rows, cell.cols))).toBe(JSON.stringify([[1,2],[3,2],[2,3],[2,1],[1,1],[3,3],[1,3],[3,1]]));
+      })
+
+      it('[0,0] adjacent cells are [[1,0],[0,1],[1,1]]', function() {
+        expect (JSON.stringify(cell.getAdjacents([0,0], cell.rows, cell.cols))).toBe(JSON.stringify([[1,0],[0,1],[1,1]]));
+      })
+
+    });
 
 });
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,3 +1,53 @@
-var Game = function(){
-  this.board = [true]
+
+var Game = function(rows, cols){
+  this.board = this.setUpBoard(rows, cols);
+  this.hello = 1;
 }
+
+// creates multi-dimensional array
+Game.prototype.setUpBoard = function (rows, cols) {
+  var arr = [];
+
+  for(var i=0; i < rows; i++){
+      arr.push([]);
+      arr[i].push( new Array(cols));
+
+      for(var j=0; j < cols; j++){
+        arr[i][j] = new Cell([i, j], rows, cols);
+      }
+  }
+  return arr;
+}
+
+// pre-populates board with selected cells and starts game 
+Game.prototype.start = function() {
+  this.board[1][5].selected = true
+}
+
+Game.prototype.countSelectedAdjacents = function(cell) {
+  var board = this.board
+  return cell.adjacents.filter(function (value) {return board[value[0]][value[1]].selected}).length
+}
+
+//cell class
+var Cell = function(location, rows, cols){
+  this.selected = false
+  this.rows = rows; this.cols = cols;
+  this.location = location;
+  this.adjacents = this.getAdjacents(location);
+}
+
+Cell.prototype.isCellOnGrid = function(cell) {
+    var x = cell[0], 
+        y = cell[1];
+    return x >= 0 && x <= this.rows && y >= 0 && y <= this.cols;
+}
+
+Cell.prototype.getAdjacents = function(cell) {
+    var x = cell[0], 
+        y = cell[1],
+        //[[W], [E], [S], [N], [NW], [ES], [EN], [NE]]
+        adjacents = [[x-1,y],[x+1,y],[x,y+1],[x,y-1],[x-1,y-1],[x+1,y+1],[x-1,y+1],[x+1,y-1]]; 
+    return adjacents.filter(this.isCellOnGrid.bind(this));
+}
+
